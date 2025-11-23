a131cecc3c80a6cc29d97ec9fb85fe2840986f85
nout <kuzovnojmaster@gmail.com>
2025-11-23 18:32:50 +0800

    added get request to bot in services.py

--- services/services.py
+++ services/services.py
@@ -1,28 +1,20 @@
-import random
+import requests
+from environs import Env
 
-from lexicon.lexicon import LEXICON_RU
+def get_request():
+    
+    env = Env()
+    env.read_env()
+    bearer_token=env("BEARER_TOKEN")
 
-
-# Функция, возвращающая случайный выбор бота в игре
-def get_bot_choice() -> str:
-    return random.choice(["rock", "paper", "scissors"])
-
-
-# Функция, возвращающая ключ из словаря, по которому
-# хранится значение, передаваемое как аргумент - выбор пользователя
-def _normalize_user_answer(user_answer: str) -> str:
-    for key in LEXICON_RU:
-        if LEXICON_RU[key] == user_answer:
-            break
-    return key
-
-
-# Функция, определяющая победителя
-def get_winner(user_choice: str, bot_choice: str) -> str:
-    user_choice = _normalize_user_answer(user_choice)
-    rules = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
-    if user_choice == bot_choice:
-        return "nobody_won"
-    elif rules[user_choice] == bot_choice:
-        return "user_won"
-    return "bot_won"
+    url = "https://kuzovnojmaster.ru/api/"
+    headers = {
+        "Authorization": f"Bearer {bearer_token}",
+        "Content-Type": "application/json"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        return str(response.status_code)
+            
+    except requests.exceptions.RequestException as e:
+        return(f"Ошибка при выполнении запроса: {e}")
